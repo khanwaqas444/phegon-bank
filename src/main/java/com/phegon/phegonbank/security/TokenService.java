a5a40e041ecd0a36802c256abfcb17f348ed67bd
khanwaqas444 <waqaskhan4447770@gmail.com>
2026-03-12 20:55:29 +0530

    security

--- src/main/java/com/phegon/phegonbank/security/TokenService.java
+++ src/main/java/com/phegon/phegonbank/security/TokenService.java
@@ -1,15 +1,30 @@
 package com.phegon.phegonbank.security;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class TokenService {
 
-    private SecretKey key;
-
+    @Value("${jwt.secret.string}")
     private String JWT_SECRET;
 
+    @Value("${jwt.expiration.time}")
     private String EXPIRATION_TIME;
+
+
+    private SecretKey key;
+
+
+    @PostConstruct
+    private void init(){
+        byte[] keyByte = JWT_SECRET.getBytes(StandardCharsets.UTF_8);
+        this.key = new SecretKeySpec(keyByte, "HmacSHA256");
+    }
+
 }
