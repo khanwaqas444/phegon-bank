47a9261da5c3bdda79b47201f36ab673d9c1cb1b
khanwaqas444 <waqaskhan4447770@gmail.com>
2026-03-15 21:03:41 +0530

    security

--- src/main/java/com/phegon/phegonbank/security/TokenService.java
+++ src/main/java/com/phegon/phegonbank/security/TokenService.java
@@ -1,5 +1,7 @@
 package com.phegon.phegonbank.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class TokenService {
@@ -25,6 +29,27 @@ public class TokenService {
     private void init(){
         byte[] keyByte = JWT_SECRET.getBytes(StandardCharsets.UTF_8);
         this.key = new SecretKeySpec(keyByte, "HmacSHA256");
+    }
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction){
+        return claimsTFunction.
+                apply(Jwts.parser().verifyWith(key).
+                        build().
+                        parseSignedClaims(token)
+                        .getPayload());
     }
 
 }
