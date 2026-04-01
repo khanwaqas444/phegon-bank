445f14020db6131d38821164890bae0119c4249e
khanwaqas444 <waqaskhan4447770@gmail.com>
2026-04-01 23:49:51 +0530

    Updated

---
+++ src/main/java/com/phegon/phegonbank/notification/services/NotificationServiceImpl.java
@@ -0,0 +1,66 @@
+package com.phegon.phegonbank.notification.services;
+
+import com.phegon.phegonbank.auth_users.entity.User;
+import com.phegon.phegonbank.notification.dtos.NotificationDTO;
+import com.phegon.phegonbank.notification.repo.NotificationRepo;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.nio.charset.StandardCharsets;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class NotificationServiceImpl implements NotificationService {
+
+    private final NotificationRepo notificationRepo;
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
+    @Override
+    @Async
+    public void sendEmail(NotificationDTO notificationDTO, User user) {
+
+        try{
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(
+                    mimeMessage,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name()
+            );
+            helper.setTo(notificationDTO.getRecipient());
+            helper.setSubject(notificationDTO.getSubject());
+
+            if (notificationDTO.getTemplateName() != null){
+                Context context = new Context();
+                context.setVariables(notificationDTO.getTemplateVariables());
+                String htmlContent = templateEngine.process(notificationDTO.getTemplateName(), context);
+                helper.setText(htmlContent, true);
+            }
+
+        }catch (MessagingException e){
+            log.error(e.getMessage());
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
